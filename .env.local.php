e7db64474c14f4a63b10731468613a612ac76b5e
Dennis Stolmeijer <dstollie@hotmail.com>
2015-09-14 22:44:31 +0200

    initiali commit

---
+++ .env.local.php
@@ -0,0 +1,15 @@
+<?php
+
+/*----------------------------------------------------*/
+// Local environment vars
+/*----------------------------------------------------*/
+return array(
+
+    'DB_NAME'       => 'wordpress_oypo2',
+    'DB_USER'       => 'root',
+    'DB_PASSWORD'   => '',
+    'DB_HOST'       => 'localhost',
+    'WP_HOME'       => 'http://oypo.dev',
+    'WP_SITEURL'    => 'http://oypo.dev/cms'
+
+);
---
+++ config/environment.php
@@ -0,0 +1,11 @@
+<?php
+
+/*----------------------------------------------------*/
+// Define your environments
+/*----------------------------------------------------*/
+return array(
+
+    'local'         => 'dennis-laptop',
+    'production'    => 'your-production-hostname'
+
+);
